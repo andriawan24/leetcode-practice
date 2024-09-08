eeb1015978657da337df9693b5e56a9a6d15dfd1
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-09-08 07:16:40 +0700

    Time: 5 ms (26.94%), Space: 10.6 MB (48.83%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -8,7 +8,8 @@ public:
             }
             digits[i] = 0;
         }
-        digits.insert(digits.begin(), 1);
+        digits.push_back(0);
+        digits[0] = 1;
         return digits;
     }
 };
