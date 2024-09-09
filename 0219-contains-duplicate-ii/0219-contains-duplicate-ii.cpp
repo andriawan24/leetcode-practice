336397b6f71dc78e0b63be37fcea10abd8b37592
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-09-09 09:28:23 +0700

    Time: 183 ms (20.07%), Space: 84.5 MB (9.15%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+       map<int, int> counts;
+
+       for (int i = 0; i < nums.size(); i++) {
+            if (counts.count(nums[i]) > 0) {
+                int result = abs(counts[nums[i]] - i);
+                cout << counts[nums[i]] << i << result << endl;
+                if (result <= k) {
+                    return true;
+                } else {
+                    counts[nums[i]] = i;
+                }
+            } else {
+                counts[nums[i]] = i;
+            }
+       } 
+
+       return false;
+    }
+};
