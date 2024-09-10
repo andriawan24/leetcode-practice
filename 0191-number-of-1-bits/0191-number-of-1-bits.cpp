e7dbbfe0afc19558c2bccdd1cc7ac796983387a7
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-09-10 23:05:01 +0700

    Time: 2 ms (37.21%), Space: 7.9 MB (78.75%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int counter = 0;
+        while (n > 0) {
+            if (n & 1) {
+                counter++;
+            }
+            n >>= 1;
+        }
+        return counter;
+    }
+};
