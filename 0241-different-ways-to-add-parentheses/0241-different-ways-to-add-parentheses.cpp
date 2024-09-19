a455310f56b11e615fa82a906ee561e6427c7c11
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-09-19 23:33:31 +0700

    Time: 0 ms (100%), Space: 9.1 MB (88.77%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
+    unordered_map<string, vector<int>> memo;
+
     vector<int> diffWaysToCompute(string expression) {
+        if (memo.find(expression) != memo.end()) {
+            return memo[expression];
+        }
+
         vector<int> res;
-        for (size_t i = 0; i < expression.length(); ++i) {
-            char operand = expression[i];
-            if (operand == '+' || operand == '-' || operand == '*') {
+
+        for (size_t i = 0; i < expression.size(); ++i) {
+            char c = expression[i];
+            if (c == '+' || c == '-' || c == '*') {
                 vector<int> leftRes = diffWaysToCompute(expression.substr(0, i));
                 vector<int> rightRes = diffWaysToCompute(expression.substr(i + 1));
 
                 for (int left: leftRes) {
                     for (int right: rightRes) {
-                        if (operand == '+') res.push_back(left + right);
-                        else if (operand == '-') res.push_back(left - right);
-                        else if (operand == '*') res.push_back(left * right);
+                        if (c == '+') res.push_back(left + right);
+                        else if (c == '-') res.push_back(left - right);
+                        else if (c == '*') res.push_back(left * right);
                     }
                 }
             }
         } 
         if (res.empty()) res.push_back(stoi(expression));
+        memo[expression] = res;
         return res;
     }
 };
