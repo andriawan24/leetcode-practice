b84ee82ef6a2ef19adb665eb62232e0bd5e2a027
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-09-23 23:08:01 +0700

    Time: 11 ms (71.69%), Space: 8.6 MB (27.97%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -2,13 +2,8 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         if (n == 1) return true;
-
-        long i = 3;
-        while (i <= n) {
-            if (i == n) return true;
-            i *= 3;
-        }
-
-        return false;
+        else if (n == 0 || n % 3 != 0) return false;
+        n /= 3;
+        return isPowerOfThree(n);
     }
 };
