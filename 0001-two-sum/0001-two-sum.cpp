c601409b8ba4dd06f5a18644416f2b3a1f5cbc9d
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-10-08 22:05:01 +0700

    Time: 10 ms (65.19%), Space: 14.6 MB (14.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> diff_table;
+        map<int, int> diff_table;
         for (int i = 0; i < nums.size(); i++) {
             int diff = target - nums[i];
             if (diff_table.find(diff) != diff_table.end()) {
