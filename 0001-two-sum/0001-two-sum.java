9c15b38fad5fe8271dd68b777a0ef99f27511ab6
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-10-08 22:11:11 +0700

    Time: 2 ms (98.76%), Space: 45.3 MB (5.37%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> table = new HashMap<>();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (table.containsKey(diff)) {
+                result[0] = i;
+                result[1] = table.get(diff);
+                return result;
+            }
+            table.put(nums[i], i);
+        }
+        return result;
+    }
+}
