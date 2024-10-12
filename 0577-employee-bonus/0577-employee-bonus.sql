7b8e68784d10c13be160b25b0d08e9b63d513aee
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-10-13 00:50:13 +0700

    Time: 833 ms (77.49%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,5 +1,1 @@
-SELECT e.name, b.bonus FROM employee e LEFT OUTER JOIN bonus b ON e.empId = b.empId WHERE b.bonus < 1000 OR b.bonus is null
-
-UNION ALL
-
-SELECT e.name, b.bonus FROM employee e RIGHT OUTER JOIN bonus b ON e.empId = b.empId WHERE b.bonus is null AND b.bonus < 1000 OR b.bonus is null;
+SELECT e.name, b.bonus FROM employee e LEFT JOIN bonus b ON e.empId = b.empId WHERE b.bonus < 1000 OR b.bonus is null;
