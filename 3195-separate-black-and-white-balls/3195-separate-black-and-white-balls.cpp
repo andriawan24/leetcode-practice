74563267e78178ed790ae8e549ed599edcaf2ad7
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-10-15 21:12:51 +0700

    Time: 33 ms (53.47%), Space: 15.7 MB (45.68%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        if (s.size() == 1) return 0;
+
+        long long moves = 0;
+        int left = 0;
+        int right = 1;
+        int n = s.size();
+
+        while (right < n) {
+            if (s[left] == '1' && s[right] == '0') {
+                swap(s[left], s[right]);
+                left++;
+                right++;
+                moves += right - left;
+            } else if (s[left] == '0' && s[right] == '0') {
+                left++;
+                right++;
+            } else if (s[left] == '0') {
+                left++;
+            } else if (s[right] == '1') {
+                right++;
+            }
+        }
+
+        cout << s << endl;
+        
+        return moves;
+    }
+};
