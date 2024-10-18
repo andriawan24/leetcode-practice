dc828c7115a11101553ca30a066371dadf64adc8
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-10-18 22:39:33 +0700

    Time: 252 ms (26.88%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,1 +1,1 @@
-SELECT * from cinema WHERE id % 2 != 0 AND description != 'boring' ORDER BY rating DESC
+SELECT id, movie, description, rating from cinema WHERE id % 2 != 0 AND description != 'boring' ORDER BY rating DESC
