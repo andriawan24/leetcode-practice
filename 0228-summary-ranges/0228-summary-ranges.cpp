dfac86ce24da3635b14f9247645b581f940d51cb
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-10-28 22:23:49 +0700

    Time: 0 ms (100%), Space: 8.5 MB (79.96%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -2,33 +2,36 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         int n = nums.size();
-        if (n == 1) return { to_string(nums[0]) };
-        if (n == 0) return { };
-        
+
+        if (n == 0) return {};
+
         vector<string> result;
-        int end = nums[n-1], last = end;
+        int end = nums[n-1], start = end;
 
         for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] + 1 == last) {
-                last = nums[i];
+            if (nums[i] + 1 == start) {
+                start = nums[i];
                 continue;
             }
 
-            if (last == end) {
-                result.insert(result.begin(), to_string(last));
-            } else {
-                result.insert(result.begin(), to_string(last) + "->" + to_string(end));
-            }
+            insert(start, end, result);
+            
+            // Reset
+            start = nums[i];
             end = nums[i];
-            last = nums[i];
         }
 
-if (last == end) {
-                result.insert(result.begin(), to_string(last));
-            } else {
-                result.insert(result.begin(), to_string(last) + "->" + to_string(end));
-            }
+        if (n > 0) insert(start, end, result);
 
         return result;
+    }
+
+    void insert(int start, int end, vector<string>& result) {
+        if (start == end) {
+            result.insert(result.begin(), to_string(start));
+        } else {
+            string text = to_string(start) + "->" + to_string(end);
+            result.insert(result.begin(), text);
+        }
     }
 };
