959dc1591d1c35bec296906fe1e5ea4129b83880
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-11-10 22:44:10 +0700

    Time: 0 ms (100%), Space: 7.8 MB (46.19%) - LeetHub

--- 3396-valid-word/3396-valid-word.cpp
+++ 3396-valid-word/3396-valid-word.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     bool isValid(string word) {
-        if (word.size() < 3) return false;
+        if (word.length() < 3) return false;
 
-        vector<char> vowels{'a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O'};
-
-        bool isContainsNonCharacter = false;
-        int vowelCount = 0;
-        int consonantCount = 0;
+        bool isVowel = false;
+        bool isConsonant = false;
 
         for (auto letter : word) {
-            int letterIdx = (int) letter;
-            if (letterIdx >= 48 && letterIdx <= 57) {
-                // Do nothing
-            } else if (find(begin(vowels), end(vowels), letter) != end(vowels)) {
-                vowelCount++;
-            } else if (letterIdx >= 65 && letterIdx <= 90) {
-                consonantCount++;
-            } else if (letterIdx >= 97 && letterIdx <= 122) {
-                consonantCount++;
+            if (isdigit(letter)) {
+                continue;
+            } 
+            
+            if (isalpha(letter)) {
+                char lower = tolower(letter);
+                if (lower == 'a' || lower == 'i' || lower == 'u' || lower == 'e' || lower == 'o') {
+                    isVowel = true;
+                } else {
+                    isConsonant = true;
+                }
             } else {
-                isContainsNonCharacter = true;
+                return false;
             }
         }
 
-        return consonantCount >= 1 && vowelCount >= 1 && !isContainsNonCharacter;
+        return isVowel && isConsonant;
     }
 };
