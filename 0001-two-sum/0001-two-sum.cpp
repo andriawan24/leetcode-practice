7b23fd7b3f05754a811fad0d5c094ebf4963a28d
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-11-16 16:32:41 +0700

    Time: 4 ms (65.37%), Space: 15 MB (10.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int> nums, int target)  {
-        unordered_map<int, int> table;
+        map<int, int> table;
         
         for (int i = 0; i < nums.size(); i++) {
             if (table.find(target - nums[i]) != table.end()) {
                 return { i, table[target - nums[i]] };
             } else {
-                table.insert({ nums[i], i });
+                table[nums[i]] = i;
             }
         }
 
