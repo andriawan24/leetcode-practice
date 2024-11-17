98330c55658e2789cec43f3af49f4aba9835b68b
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-11-17 11:11:41 +0700

    Time: 2 ms (100%), Space: 44.7 MB (56.46%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,41 +1,45 @@
 class Solution {
     public int romanToInt(String s) {
-        int sum = 0;
-        char prevChar = ' ';
+        int sum = getNumber(s.charAt(0), ' ');
 
-        for (int i = 0; i < s.length(); i++) {
-            switch (s.charAt(i)) {
-                case 'I':
-                    sum += 1;
-                    break;
-                case 'V':
-                    if (prevChar == 'I') sum -= 2;
-                    sum += 5;
-                    break;
-                case 'X':
-                    if (prevChar == 'I') sum -= 2;
-                    sum += 10;
-                    break;
-                case 'L':
-                    if (prevChar == 'X') sum -= 20;
-                    sum += 50;
-                    break;
-                case 'C':
-                    if (prevChar == 'X') sum -= 20;
-                    sum += 100;
-                    break;
-                case 'D':
-                    if (prevChar == 'C') sum -= 200;
-                    sum += 500;
-                    break;
-                case 'M':
-                    if (prevChar == 'C') sum -= 200;
-                    sum += 1000;
-                    break;
-            }
-            prevChar = s.charAt(i);
+        for (int i = 1; i < s.length(); i++) {
+            sum += getNumber(s.charAt(i), s.charAt(i-1));
         }
 
+        return sum;
+    }
+
+    public int getNumber(char roman, char prev) {
+        int sum = 0;
+        switch (roman) {
+            case 'I':
+                sum += 1;
+                break;
+            case 'V':
+                if (prev == 'I') sum -= 2;
+                sum += 5;
+                break;
+            case 'X':
+                if (prev == 'I') sum -= 2;
+                sum += 10;
+                break;
+            case 'L':
+                if (prev == 'X') sum -= 20;
+                sum += 50;
+                break;
+            case 'C':
+                if (prev == 'X') sum -= 20;
+                sum += 100;
+                break;
+            case 'D':
+                if (prev == 'C') sum -= 200;
+                sum += 500;
+                break;
+            case 'M':
+                if (prev == 'C') sum -= 200;
+                sum += 1000;
+                break;
+        }
         return sum;
     }
 }
