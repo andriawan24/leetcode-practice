14c5198b800c84b80aec61d3ec23568ace4f7f97
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-11-19 18:24:37 +0700

    Time: 203 ms (22.14%), Space: 104.1 MB (18.84%) - LeetHub

---
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        long long result = 0;
+        long long current_sum = 0;
+        set<int> elements;
+        int start = 0;
+
+        for (int end = start; end < nums.size(); end++) {
+            if (elements.find(nums[end]) == elements.end()) {
+                current_sum += nums[end];
+                elements.insert(nums[end]);
+
+                if (end - start + 1 == k) {
+                    result = max(current_sum, result);
+                    current_sum -= nums[start];
+                    elements.erase(nums[start]);
+                    start++;
+                }
+            } else {
+                while (nums[start] != nums[end]) {
+                    current_sum -= nums[start];
+                    elements.erase(nums[start]);
+                    start++;
+                }
+                start++;
+            }
+        }
+
+        return result;
+    }
+};
