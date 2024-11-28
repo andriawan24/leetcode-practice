e711baa545c3e2b707c5cd81bbf21b110aa706ec
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-11-28 22:56:03 +0700

    Time: 12 ms (96.89%), Space: 38.1 MB (88.06%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        string result;
-        int lastCharCount = 0;
-        char lastChar = ' ';
+        int writePos = 0;
+        int count = 1;
 
-        for (int i = 0; i < s.size(); i++) {
-            if (lastChar != s[i]) {
-                lastChar = s[i];
-                lastCharCount = 1;
-                result += lastChar;
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] == s[writePos]) {
+                count++;
             } else {
-                lastCharCount++;
-                if (lastCharCount < 3) {
-                    result += lastChar;
-                }
+                count = 1;
+            }
+
+            if (count <= 2) {
+                s[++writePos] = s[i];
             }
         }
-
-        return result;
+        s.resize(writePos + 1);
+        return s;
     }
 };
