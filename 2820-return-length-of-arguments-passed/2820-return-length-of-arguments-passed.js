69e0c2e702ccec34fdc8aabf3b8b11b11f79a7a8
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-12-28 11:04:37 +0700

    Time: 42 ms (94.61%), Space: 48 MB (98.66%) - LeetHub

--- 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -3,6 +3,4 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-var argumentsLength = function(...args) {
-    return args.length;
-};
+var argumentsLength = (...args) => args.length;
