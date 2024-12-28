5e5545129efe4793ebe4cc1deed1f60ecb31fe1a
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-12-28 10:54:36 +0700

    Time: 5 ms (2.6%), Space: 42.3 MB (6.3%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.java
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -1,18 +1,5 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int result = 0;
-        HashMap<Character, Integer> tables = new HashMap<Character, Integer>();
-
-        for (char jewel: jewels.toCharArray()) {
-            tables.put(jewel, 1);
-        }
-        
-        for (char stone : stones.toCharArray()) {
-            if (tables.get(stone) != null) {
-                result++;
-            }
-        }
-
-        return result;
+        return stones.replaceAll("[^" + jewels + "]", "").length();
     }
 }
