63f580ad4720e438ba39eae17e48265dc0e48e06
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-12-28 11:01:36 +0700

    Time: 56 ms (29.86%), Space: 49.2 MB (21.29%) - LeetHub

---
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -0,0 +1,8 @@
+/**
+ * Try to solve most highest acceptance rate on Leetcode
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
