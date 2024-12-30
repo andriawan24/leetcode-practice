a1a3970908ea37c50c9e5ea68c47bf842be6d0ba
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-12-30 21:53:29 +0700

    Time: 0 ms (100%), Space: 9.7 MB (29.5%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -3,7 +3,7 @@ public:
     int numIdenticalPairs(vector<int>& nums) {
         int result = 0;
 
-        for (int i = 0; i < nums.size() - 1; i++) {
+        for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) {
                 if (nums[i] == nums[j]) result++;
             }
