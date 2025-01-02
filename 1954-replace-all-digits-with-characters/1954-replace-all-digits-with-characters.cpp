32f2f8a144ebbcaf037b5dbdf8330027552d58e9
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-01-02 21:07:33 +0700

    Time: 0 ms (100%), Space: 8.2 MB (28.8%) - LeetHub

---
+++ 1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string replaceDigits(string s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 != 0) {
+                s[i] = (char) (((int) s[i - 1]) + (s[i] - '0'));
+            }
+        }
+
+        return s;
+    }
+};
