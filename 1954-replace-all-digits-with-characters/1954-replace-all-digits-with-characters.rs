7248cf221e71abfb313fd4193a327468c03934c6
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-01-02 22:17:00 +0700

    Time: 0 ms (100%), Space: 2.3 MB (35.71%) - LeetHub

--- 1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.rs
+++ 1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.rs
@@ -1,27 +1,14 @@
 impl Solution {
-    pub fn get_ascii(s: &String, index: usize) -> Option<u8> {
-        s.chars().nth(index).map(|c| c as u8)
-    }
-
     pub fn replace_digits(s: String) -> String {
-        let mut chars: Vec<char> = s.chars().collect();
-    
-        for i in (1..s.len()).step_by(2) {
-            let mut converted_char = 'a';
-            let prev_char = Solution::get_ascii(&s, i - 1);
-            let current_char = chars[i].to_digit(10);
+        let mut res = vec![];
 
-            if let Some(prev_char_value) = prev_char {
-                if let Some(current_char_value) = current_char {
-                    if current_char_value <= u8::MAX as u32 {
-                        converted_char = (current_char_value as u8 + prev_char_value) as char
-                    }
-                }
+        for chunk in s.as_bytes().chunks(2) {
+            res.push(chunk[0]);
+            if chunk.len() == 2 {
+                res.push(chunk[0] + (chunk[1] - b'0'));
             }
-            
-            chars[i] = converted_char;
         }
 
-        return chars.iter().collect()
+        String::from_utf8(res).unwrap_or_default()
     }
 }
