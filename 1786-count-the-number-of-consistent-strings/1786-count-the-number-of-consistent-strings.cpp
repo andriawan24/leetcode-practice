c71352546deedfc1a36aaae7c4283559de49cf79
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-01-05 19:57:47 +0700

    Time: 35 ms (90.2%), Space: 34 MB (34.89%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+
         int count = 0;
 
         for (int i = 0; i < words.size(); i++) {
