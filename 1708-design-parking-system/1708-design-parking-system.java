fbf9865dfca87544d92eab4e64b10fd0d79a9dcd
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-01-06 21:55:27 +0700

    Time: 7 ms (100%), Space: 45 MB (93.33%) - LeetHub

---
+++ 1708-design-parking-system/1708-design-parking-system.java
@@ -0,0 +1,33 @@
+class ParkingSystem {
+    private int _big = 0;
+    private int _medium = 0;
+    private int _small = 0;
+
+    public ParkingSystem(int big, int medium, int small) {
+        _big = big;
+        _medium = medium;
+        _small = small;
+    }
+    
+    public boolean addCar(int carType) {
+        switch (carType) {
+            case 1:
+                _big--;
+                return _big >= 0;
+            case 2:
+                _medium--;
+                return _medium >= 0;
+            case 3:
+                _small--;
+                return _small >= 0;
+        }
+
+        return true;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
