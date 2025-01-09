39bc3a9d64d517161b199c175a6cf3b0f731922e
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-01-09 19:07:59 +0700

    Time: 2 ms (97.38%), Space: 44 MB (49.23%) - LeetHub

---
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public double minimumAverage(int[] nums) {
+        Arrays.sort(nums);
+        double minAverage = (nums[0] + nums[nums.length - 1]) / 2.0;
+
+        for (int i = 1; i < nums.length / 2; i++) {
+            double avg = (nums[i] + nums[nums.length - i - 1]) / 2.0;
+            minAverage = Math.min(minAverage, avg);
+        }
+
+        return minAverage;
+    }
+}
