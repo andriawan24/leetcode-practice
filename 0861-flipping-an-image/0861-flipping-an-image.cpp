f6292aa35b30faad1509a9a8b8f9f76e60113128
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-01-12 16:29:40 +0700

    Time: 0 ms (100%), Space: 12.7 MB (25.1%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.cpp
+++ 0861-flipping-an-image/0861-flipping-an-image.cpp
@@ -4,11 +4,7 @@ public:
         for (vector<int>& images: image) {
             std::ranges::reverse(images);
             for (int& i: images) {
-                if (i == 0) {
-                    i = 1;
-                } else {
-                    i = 0;
-                }
+                i = i == 0 ? 1 : 0;
             }
         }
 
