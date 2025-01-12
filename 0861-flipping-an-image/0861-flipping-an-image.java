57830d46d738686802aeaccdccfd54b0721b0730
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-01-12 16:36:48 +0700

    Time: 0 ms (100%), Space: 44.7 MB (36.29%) - LeetHub

---
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for (int[] images: image) {
+            int start = 0, end = images.length-1;
+            while (start < end) {
+                images[start] = (images[start] + images[end]) - (images[end] = images[start]);
+                start++;
+                end--;
+            }
+        }
+
+        for (int[] images: image) {
+            for (int i = 0; i < images.length; i++) {
+                images[i] = images[i] == 0 ? 1 : 0;
+            }
+        }
+
+        return image;
+    }
+}
