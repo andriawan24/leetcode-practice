69f689d9d56208a0d82d102e486f04b75f514377
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-01-12 16:31:52 +0700

    Time: 0 ms (100%), Space: 12.6 MB (25.1%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.cpp
+++ 0861-flipping-an-image/0861-flipping-an-image.cpp
@@ -2,7 +2,15 @@ class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
         for (vector<int>& images: image) {
-            std::ranges::reverse(images);
+            int start = 0, end = images.size()-1;
+            while (start < end) {
+                swap(images[start], images[end]);
+                start++;
+                end--;
+            }
+        }
+
+        for (vector<int>& images: image) {
             for (int& i: images) {
                 i = i == 0 ? 1 : 0;
             }
