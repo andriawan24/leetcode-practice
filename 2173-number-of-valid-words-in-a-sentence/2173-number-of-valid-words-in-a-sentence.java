abc7609f4f605ad8cc6adcbbb09e0a3d008ffa86
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-01-13 22:15:10 +0700

    Time: 8 ms (75.81%), Space: 44.5 MB (58.06%) - LeetHub

--- 2173-number-of-valid-words-in-a-sentence/2173-number-of-valid-words-in-a-sentence.java
+++ 2173-number-of-valid-words-in-a-sentence/2173-number-of-valid-words-in-a-sentence.java
@@ -1,37 +1,47 @@
 class Solution {
     public int countValidWords(String sentence) {
-        int valid = 0;
-        String[] words = sentence.split(" ");
+        int ans = 0;
+        String[] words = sentence.trim().split("\\s+");
 
-        outer: 
         for (String word: words) {
-            char[] c = word.toCharArray();
-            int countHypen = 0;
-            boolean isValid = false;
-            for (int i = 0; i < c.length; i++) {
-                if (Character.isLetter(c[i])) isValid = true;
-                if (Character.isDigit(c[i])) continue outer;
-                if (c[i] == '.' || c[i] == ',' || c[i] == '!') {
-                    if (i != c.length-1) {
-                        continue outer;
-                    } else if (c.length > 2 && c[i - 1] == '-') {
-                        continue outer;
-                    } else {
-                        isValid = true;
-                    }
-                }
-                if (c[i] == '-') {
-                    countHypen++;
-                    if (i == 0 || i == c.length-1) continue outer;
-                    if (countHypen == 2) continue outer;
-                }
-            }
-            if (isValid) {
-                System.out.println(word);
-                valid++;
+            if (isValid(word)) {
+                ans++;
             }
         }
 
-        return valid;
+        return ans;
+    }
+
+    private boolean isValid(String word) {
+        int n = word.length();
+        boolean hasPunc = false;
+        boolean hasHypen = false;
+
+        for (int i = 0; i < n; i++) {
+            char c = word.charAt(i);
+
+            if (Character.isDigit(c)) return false;
+
+            if (c == '-') {
+                if (hasHypen || i == 0 || i == n - 1) {
+                    return false;
+                }
+
+                if (!Character.isLetter(word.charAt(i-1)) || 
+                    !Character.isLetter(word.charAt(i+1))) {
+                    return false;
+                }
+                hasHypen = true;
+            } else if (c == '!' || c == '.' || c == ',') {
+                if (hasPunc || i != n - 1) {
+                    return false;
+                }
+                hasPunc = true;
+            } else if (!Character.isLetter(c)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
