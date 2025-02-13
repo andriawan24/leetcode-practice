c1c8ca6888cc600b902599cd1192106380144a71
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-02-13 11:07:08 +0700

    Time: 0 ms (100%), Space: 28.7 MB (47.41%) - LeetHub

--- 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.cpp
+++ 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.cpp
@@ -4,7 +4,7 @@ public:
         int sumSingle = 0;
         int sumDouble = 0;
 
-        for (auto num: nums) {
+        for (auto& num: nums) {
             if (num < 10) sumSingle += num;
             else sumDouble += num;
         }
