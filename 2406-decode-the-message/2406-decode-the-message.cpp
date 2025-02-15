4c34b16e8202bc7e65a396ddf67ba95003d70f56
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-02-15 10:47:32 +0700

    Time: 0 ms (100%), Space: 9.6 MB (81.07%) - LeetHub

---
+++ 2406-decode-the-message/2406-decode-the-message.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        map<char, char> table;
+        string decoded;
+
+        for (auto ch: key) {
+            if (ch != ' ' && !table[ch]) {
+                table[ch] = char('a' + table.size() - 1);
+            }
+        }
+
+        for (auto ch: message) {
+            if (ch == ' ') {
+                decoded += ch;
+            } else {
+                decoded += table[ch];
+            }
+        }
+
+        return decoded;
+    }
+};
