172f3d39cdab5fbf5cd829c27c2629e9e3fe1f4f
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-02-20 22:32:48 +0700

    Time: 0 ms (100%), Space: 8.7 MB (45.14%) - LeetHub

--- 2226-rings-and-rods/2226-rings-and-rods.cpp
+++ 2226-rings-and-rods/2226-rings-and-rods.cpp
@@ -8,22 +8,37 @@ public:
         }
 
         for (int i = 0; i < rings.size(); i+=2) {
-            if (rings[i] == 'R') {
-                rods[rings[i + 1] - '0'][0] = true;
-            } else if (rings[i] == 'G') {
-                rods[rings[i + 1] - '0'][1] = true;
-            } else if (rings[i] == 'B') {
-                rods[rings[i + 1] - '0'][2] = true;
+            int rgb = 0;
+
+            switch (rings[i]) {
+                case 'R':
+                    rgb = 0;
+                    break;
+                case 'G':
+                    rgb = 1;
+                    break;
+                case 'B':
+                    rgb = 2;
+                    break;
             }
+
+            rods[rings[i + 1] - '0'][rgb] = true;
         }
 
         int count = 0;
         for (const auto& [key, values]: rods) {
             bool isValid = true;
-            for (auto value: values) {
-                if (!value) isValid = false;
+            
+            for (const auto& value: values) {
+                if (!value) {
+                    isValid = false;
+                    break;
+                }
             }
-            if (isValid) count++;
+
+            if (isValid) {
+                count++;
+            }
         }
 
         return count;
