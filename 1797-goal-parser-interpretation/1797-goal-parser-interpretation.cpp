86ae8021fc021b48a2e4849117aa0aa96b7a75aa
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-02-27 08:15:37 +0700

    Time: 3 ms (42.99%), Space: 8.2 MB (75.8%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.cpp
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.cpp
@@ -5,18 +5,20 @@ public:
         
         int i = 0;
         int n = command.size();
+
         while (i < n) {
             if (command[i] == 'G') {
                 result += "G";
                 i++;
-            } else if (command[i] == '(') {
-                if (command[i + 1] == ')') {
-                    result += "o";
-                    i+=2;
-                } else if (command[i + 1] == 'a') {
-                    result += "al";
-                    i+=4;
-                }
+                continue;
+            }
+
+            if (command[i + 1] == ')') {
+                result += "o";
+                i+=2;
+            } else if (command[i + 1] == 'a') {
+                result += "al";
+                i+=4;
             }
         }
 
