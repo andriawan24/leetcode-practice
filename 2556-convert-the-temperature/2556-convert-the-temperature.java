30a9a6107646289da027362726ca4283985cbaa0
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-03-16 15:57:26 +0700

    Time: 0 ms (100%), Space: 42.4 MB (7.15%) - LeetHub

--- 2556-convert-the-temperature/2556-convert-the-temperature.java
+++ 2556-convert-the-temperature/2556-convert-the-temperature.java
@@ -1,8 +1,7 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        double[] result = new double[2];
-        result[0] = celsius + 273.15;
-        result[1] = celsius * 1.80 + 32.00;
-        return result;
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32.00;
+        return new double[] { kelvin, fahrenheit };
     }
 }
