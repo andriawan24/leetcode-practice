4f70a1da0782554844d1ccf2da048848df9e6e91
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-03-16 15:37:57 +0700

    Time: 0 ms (100%), Space: 42.3 MB (63.16%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int result = 0;
+
+        for (int hour: hours) {
+            if (hour >= target) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
