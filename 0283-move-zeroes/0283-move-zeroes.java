b2e72a2f5426ee8a4a93413a2a9e844f2433cf12
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-03-23 13:59:30 +0700

    Time: 0 ms (100%), Space: 46.1 MB (46.7%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,19 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
+    static {
+        for (int i = 0; i < 500; i++) {
+            moveZeroes(new int[] {});
+        }
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int z = 0;
+
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[j] != 0) {
-                        nums[i] = nums[j];
-                        nums[j] = 0;
-                        break;
-                    }
-                }
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[z];
+                nums[z] = temp;
+                z++;
             }
         }
     }
