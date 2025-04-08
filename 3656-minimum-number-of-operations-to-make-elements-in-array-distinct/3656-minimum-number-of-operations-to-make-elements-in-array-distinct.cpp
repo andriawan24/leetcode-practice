9e22dfd84fbc517241015e7a1227c7baa23fcf9d
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-04-08 15:23:11 +0700

    Time: 0 ms (100%), Space: 27.1 MB (94.71%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        int steps = 0, temp;
+        bool checks[101] = {0};
+        int n = nums.size();
 
-        while (true) {
-            unordered_map<int, int> table;
-            temp = 0;
-            for (const auto &num: nums) {
-                if (++table[num] == 2) temp++;
+        for (int i = n - 1; i >= 0; i--) {
+            if (checks[nums[i]]) {
+                return ceil(double(i + 1) / 3);
             }
-            if (temp == 0) break;
-            nums.erase(nums.begin(), nums.begin() + min(3, (int) nums.size()));
-            steps++;
+            checks[nums[i]] = true;
         }
 
-        return steps;
+        return 0;
     }
 };
