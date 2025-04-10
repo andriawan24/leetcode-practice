a25caeaa45e6257547a1d6d3abeb3c3765edc48f
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-04-10 22:06:53 +0700

    Time: 0 ms (100%), Space: 45.1 MB (92.83%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        boolean[] checks = new boolean[nums.length + 1];
-
+        int n = nums.length;
+        int sum = (n * (n + 1)) / 2;
+        int s = 0;
         for (int num: nums) {
-            checks[num] = true;
+            s += num;
         }
-
-        for (int i = 0; i < checks.length; i++) {
-            if (!checks[i]) {
-                return i;
-            }
-        }
-
-        return -1;
+        return sum - s;
     }
 }
