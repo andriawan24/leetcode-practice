ca42f21210ac193c81236db46aa13124eb5ac105
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-09-11 23:07:04 +0700

    Time: 0 ms (100%), Space: 8.7 MB (48.08%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
-        int n = columnTitle.size();
-        int total = 0;
+        int result = 0;
 
-        for (int i = n - 1; i >= 0; i--) {
-            int alphanum = (columnTitle[i] - 'A') + 1;
-            total += alphanum * pow(26, n - i - 1);
+        for (const auto title: columnTitle) {
+            int num = title - 'A' + 1;
+            result = result * 26 + num;
         }
-
-        return total;
+        
+        return result;
     }
 };
