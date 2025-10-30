13334726b7853eb22fb34648a049472dad05f419
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-10-30 10:57:00 +0700

    Time: 0 ms (100%), Space: 20.5 MB (98.05%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int result = 0;
-        for (const auto num: nums) {
+        for (const auto &num: nums) {
             result ^= num;
         }
         return result;
