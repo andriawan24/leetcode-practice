25a28d6b3d223cbbcbfeaeca1025f48e05a1f9c1
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2025-11-17 21:48:29 +0700

    Time: 0 ms (100%), Space: 9.8 MB (31.61%) - LeetHub

--- LeetCode/Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ LeetCode/Easy/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -4,25 +4,7 @@ public:
         if (str1 + str2 != str2 + str1) {
             return "";
         }
-        
-        string result = "";
-
-        int a = str1.size();
-        int b = str2.size();
-
-        while (b > 0) {
-            int temp = a;
-            a = b;
-            b = temp % b;
-        }
-
-        for (int i = 0; i < a; i++) {
-            if (str1[i] != str2[i]) {
-                return "";
-            }
-            result += str1[i];
-        }
-
-        return result;
+        int prefixLength = __gcd(str1.length(),str2.length());
+        return str1.substr(0,prefixLength);
     }
 };
