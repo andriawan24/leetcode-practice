5fca2bf6deb8d6baf2237d19ffa90498a356f105
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2026-02-01 23:09:44 +0700

    Time: 8 ms (51.75%), Space: 19.8 MB (35.44%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,28 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        cleaned = ''.join([i.lower() for i in s if i.isalnum()])
-        rev = cleaned[::-1]
-        return cleaned == rev
+        start = 0
+        end = len(s) - 1
+
+        while start < end:
+            left_ch = s[start]
+            right_ch = s[end]
+
+            if s[start].isalnum():
+                left_ch = left_ch.lower()
+            else:
+                start += 1
+                continue
+
+            if s[end].isalnum():
+                right_ch = right_ch.lower()
+            else:
+                end -= 1
+                continue
+
+            if left_ch == right_ch:
+                start += 1
+                end -= 1
+            else:
+                return False
+
+        return True
