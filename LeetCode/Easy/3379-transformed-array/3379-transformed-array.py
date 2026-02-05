ab9107ad8e3232c1c149fb5c8ef641b2185c7a15
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2026-02-05 12:54:08 +0700

    Time: 66 ms (21.08%), Space: 19.4 MB (34.53%) - LeetHub

--- LeetCode/Easy/3379-transformed-array/3379-transformed-array.py
+++ LeetCode/Easy/3379-transformed-array/3379-transformed-array.py
@@ -1,9 +1,10 @@
 class Solution:
     def constructTransformedArray(self, nums: List[int]) -> List[int]:
-        res = [0] * len(nums)
+        res = []
+        n = len(nums)
 
-        for i in range(0, len(res)):
-            target_i = (i + nums[i]) % len(nums)
-            res[i] = nums[target_i]
+        for i in range(0, n):
+            target_i = (i + nums[i]) % n
+            res.append(nums[target_i])
 
         return res
