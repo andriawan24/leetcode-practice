6742cd5a730ddec475fc3cca40547399d3820b75
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2026-02-06 14:11:26 +0700

    Time: 12 ms (53.34%), Space: 19.3 MB (51.33%) - LeetHub

---
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        table = {}
+
+        for i in s:
+            if i not in table:
+                table[i] = 1
+            else:
+                table[i] += 1
+
+        for i in t:
+            if i not in table:
+                return False
+
+            if table[i] == 0:
+                return False
+
+            table[i] -= 1
+
+        return True
