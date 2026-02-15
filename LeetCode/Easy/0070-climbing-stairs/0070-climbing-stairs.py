3558e3b77bcb341b48c851af14efca0a7305c661
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2026-02-15 18:05:14 +0700

    Time: 0 ms (100%), Space: 19.5 MB (12.72%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.py
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
+        table = [0] * (n + 1)
+        table[0] = 1
+        table[1] = 2
 
-        a = 0
-        b = 1
-        res = 0
+        for i in range(2, n):
+            table[i] = table[i - 1] + table[i - 2]
 
-        for i in range(n):
-            res = a + b
-            a = b
-            b = res
-
-        return res
+        return table[-2]
