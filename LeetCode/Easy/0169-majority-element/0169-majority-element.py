530025961f98bcbef0acaba3d73ccdd44b637bed
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2026-02-24 11:21:16 +0700

    Time: 15 ms (18.92%), Space: 21.5 MB (18.13%) - LeetHub

---
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = {}
+
+        for num in nums:
+            count[num] = 1 if num not in count else count[num] + 1
+            if count[num] > len(nums) / 2:
+                return num
+
+        return 0
