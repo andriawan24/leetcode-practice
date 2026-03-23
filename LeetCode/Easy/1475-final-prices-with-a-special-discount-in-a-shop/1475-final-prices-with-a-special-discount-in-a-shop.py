e5c0c21124c0d704bb30e21eccc9f8853637b464
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2026-03-23 13:15:11 +0700

    Time: 0 ms (100%), Space: 19.2 MB (87.71%) - LeetHub

--- LeetCode/Easy/1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ LeetCode/Easy/1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,9 +1,9 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        for i in range(len(prices)):
-            for j in range(i + 1, len(prices)):
-                if prices[j] <= prices[i]:
-                    prices[i] -= prices[j]
-                    break
+        res = []
+        for i, price in enumerate(prices):
+            while res and prices[res[-1]] >= price:
+                prices[res.pop()] -= price
+            res.append(i)
 
         return prices
